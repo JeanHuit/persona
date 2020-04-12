a7ba465f639c7fbc2b1a1bc4c80833af3bdc44fb
Jean Huit <kollecter@gmail.com>
2020-04-12 17:48:22 +0000

    adds definition skill - pointer from @davidesantangelo

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'telegram_bot'
 require 'dotenv'
+require 'redis'
 require_relative './skills/dictionary'
 
 Dotenv.load
@@ -10,24 +11,29 @@ bot.get_updates(fail_silently: true) do |message|
   puts "@#{message.from.username}: #{message.text}"
   command = message.get_command_for(bot)
 
+  last_word = Redis.new(host: 'localhost')
+
   get_meaning = GenerateMeaning.new
 
   message.reply do |reply|
     case command
-    when /greet/i
+    when /start/i
       reply.text = "Hello, #{message.from.first_name}!"
     when /define/i
-      reply.text = "What word do you want my help with?"
-      # reply.text = get_meaning.meaning(message)
+      last_word.set('comm', command)
+      reply.text = 'What word do you want my help with?'
+    when /stop/i
+      reply.text = 'See you later'
     else
-      reply.text = "#{message.from.first_name}, have no idea what #{command.inspect} means."
+      case command
+      when last_word.get('comm') == '/define' && command
+        reply.text = get_meaning.meaning(command)
+        last_word.set('comm', 'done')
+      else
+        reply.text = "#{message.from.first_name}, have no idea what #{command.inspect} means in this context, use: /help"
+      end
     end
     puts "sending #{reply.text.inspect} to @#{message.from.username}"
     reply.send_with(bot)
   end
 end
-
-      # wprd= gets.chomp
-      # get_meaning = GenerateMeaning.new
-      # result = get_meaning.meaning(word)
-      # bot.api.send_message(chat_id: message.chat.id, text: result.to_s)
