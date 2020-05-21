be77aff3b961737739528d40eaa1b8b46d725e96
Jean Huit <kollecter@gmail.com>
2020-05-21 09:58:40 +0000

    adds 'new skill' local news headlines via command /heandlines

--- skills/news.rb
+++ skills/news.rb
@@ -24,15 +24,27 @@ class GenerateNews
   end
 
   def rss_feed
-    news_feed = Hash.new
+    ness = []
+    news_feed = []
     number = 0
     url = 'https://cdn.ghanaweb.com/feed/newsfeed.xml'
     feed = RSS::Parser.parse(URI.open(url).read)
     puts "Title: #{feed.channel.title}"
     feed.items.each do |item|
-      news_feed[number] = item.title.to_s + "\n" + item.description.to_s + "\n" + item.link.to_s
+      news_feed[number] = '*' + item.title.to_s
       number += 1
     end
-    news_feed[1]
+    if news_feed.join.length > 4096
+      (0...25).each do |num|
+        ness << news_feed[num]
+      end
+      ness.join("\n")
+      (26...50).each do |num|
+        ness << news_feed[num]
+      end
+      ness.join("\n")
+    else
+      news_feed.join("\n")
+    end
   end
 end
