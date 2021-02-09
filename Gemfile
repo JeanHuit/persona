b4759aebe0a4d7c0564562acd079263cff62c742
Jean Huit <kollecter@gmail.com>
2021-02-09 15:01:31 +0000

    gtesting this code

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ gem 'json'
 gem 'telegram-bot-ruby'
 gem 'redis'
 gem 'net-http-persistent'
+gem "sinatra"
+gem "sinatra-contrib"
--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'telegram/bot'
 require 'dotenv'
+require 'sinatra'
+require 'sinatra/reloader'
 require 'redis'
 require 'net/http/persistent'
 require_relative './skills/dictionary'
@@ -14,30 +16,40 @@ Telegram::Bot.configure do |config|
   config.adapter = :net_http_persistent
 end
 
-last_word = Redis.new(host: '127.0.0.1')
-get_meaning = GenerateMeaning.new
-rss_feed = GenerateNews.new
+# last_word = Redis.new(host: '127.0.0.1')
+# get_meaning = GenerateMeaning.new
+# rss_feed = GenerateNews.new
 
-Telegram::Bot::Client.run(token) do |bot|
-  bot.listen do |message|
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-    when '/define'
-      last_word.set('comm', message.text)
-      bot.api.send_message(chat_id: message.chat.id, text: 'What word do you want my help with?')
-    when '/headlines'
-      bot.api.send_message(chat_id: message.chat.id, text: rss_feed.rss_feed)
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-    else
-      case message.text
-      when last_word.get('comm') == '/define' && message.text
-        bot.api.send_message(chat_id: message.chat.id, text: get_meaning.meaning(message.text)) 
-        last_word.set('comm', 'done')
-      else
-        bot.api.send_message(chat_id: message.chat.id, text: " have no idea what #{message.inspect} means in this context, use: /help")
+get '/start' do
+  Telegram::Bot::Client.run(token) do |bot|
+    bot.listen do |message|
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
       end
     end
   end
-end
+  
+  # erb :index
+
+# Telegram::Bot::Client.run(token) do |bot|
+#   bot.listen do |message|
+#     case message.text
+#     when '/start'
+#       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+#     when '/define'
+#       last_word.set('comm', message.text)
+#       bot.api.send_message(chat_id: message.chat.id, text: 'What word do you want my help with?')
+#     when '/headlines'
+#       bot.api.send_message(chat_id: message.chat.id, text: rss_feed.rss_feed)
+#     when '/stop'
+#       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+#     else
+#       case message.text
+#       when last_word.get('comm') == '/define' && message.text
+#         bot.api.send_message(chat_id: message.chat.id, text: get_meaning.meaning(message.text)) 
+#         last_word.set('comm', 'done')
+#       else
+#         bot.api.send_message(chat_id: message.chat.id, text: " have no idea what #{message.inspect} means in this context, use: /help")
+#       end
+#     end
+#   end
+# end
