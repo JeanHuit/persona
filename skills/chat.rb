fa8b8b124eb418d97341598cbd1b1e70dc74281d
Jean Huit <kollecter@gmail.com>
2021-02-26 11:11:18 +0000

    selects a bot based on the time of day

--- skills/chat.rb
+++ skills/chat.rb
@@ -9,14 +9,23 @@ Dotenv.load
 # Begin the chat, with a randomized selection of bots
 class BeginConversation
   def choosebot
-    arr = [71367, 63906, 147504, 6, 6526, 148149, 78951, 118099, 159151, 64022, 150647, 92626]
-    arr[rand(11)]
+    time = Time.new
+    arr = [71367, 63906, 147504, 6, 6526, 148149, 78951]
+    # arr[rand(11)]
+    #  118099, 159151, 64022, 150647, 92626
+    # chooses a bot based on the day
+    arr[time.wday]
   end
 
   def converse(chat)
     key = ENV['chat_key']
     url = "https://www.personalityforge.com/api/chat/?apiKey=#{key}&chatBotID=#{choosebot}&message=#{chat}&externalID=ntikuma07"
     content = JSON.parse(URI.open(url).string)
+  rescue NoMethodError
+    puts 'this is not right'
+  rescue => e
+    print_exception(e, false)
+  else
     content['message']['message']
   end
 end
