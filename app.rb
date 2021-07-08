639b24394a1988500bad4784c93211bf6ca02013
Jean Huit <kollecter@gmail.com>
2021-07-08 14:00:43 +0000

    adds code to workout nil class

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ talk = BeginConversation.new
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    case message.text
+    case message&.text
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
     when '/define'
